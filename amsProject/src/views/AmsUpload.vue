07f2fd272ceabb50e0f1ddb6b141cf56eb73b4a5
冯亮 <feng_liang01@hoperun.com>
2019-06-05 17:35:57 +0800

    上传文件组件提交
    上传文件组件提交

--- amsProject/src/views/AmsUpload.vue
+++ amsProject/src/views/AmsUpload.vue
@@ -1,22 +1,23 @@
 <template>
 	<el-row class="container">
 		<el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
-			<el-form :model="uploadData">
+			<el-form :inline="true" :model="uploadData">
 				<el-form-item label="上传类型">
-					<el-input v-model="uploadData.upLoadType" placeholder="上传类型"></el-input>
+					<el-select v-model="uploadData.upLoadType" placeholder="请选择上传类型" >
+						<el-option v-for="item in uploadTypeOptions" :key="item" :label="item" :value="item"></el-option>
+					</el-select>
 				</el-form-item>
 				<el-form-item label="许可证号">
 					<el-input v-model="uploadData.prjSN" placeholder="许可证号"></el-input>
 				</el-form-item>
-				<el-form-item label="长传文件">
-					<el-upload class="upload-demo" ref="upload" :file-list="files" :auto-upload="false"
-					:on-preview="handlePreview" :on-remove="handleRemove" >
+			</el-form>
+			<div>
+				<el-upload class="upload-demo" action="" :auto-upload="false" multiple  :on-change="fileChange" >
 					<el-button slot="trigger" size="small" type="primary">选取文件</el-button>
 					<el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
-					<div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+					<div slot="tip" class="el-upload__tip">只能上传excel文件</div>
 				</el-upload>
-				</el-form-item>
-			</el-form>
+			</div>
 		</el-col>
 	</el-row>
 </template>
@@ -36,13 +37,11 @@
 		},
 		methods: {
 			submitUpload() {
+				console.log(this.uploadData.files);
 				debugger;
 			},
-			handleRemove(file, fileList) {
-				console.log(file, fileList);
-			},
-			handlePreview(file) {
-				console.log(file);
+			fileChange(file, fileList) {
+				this.uploadData.files = fileList;
 			}
 		}
 	}
