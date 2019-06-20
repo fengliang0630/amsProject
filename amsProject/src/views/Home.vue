cd13ff70540966024ea5c96b2b0ad343b6745356
冯亮 <135026209@qq.com>
2019-06-20 20:39:26 +0800

    版本更新
    版本更新

--- amsProject/src/views/Home.vue
+++ amsProject/src/views/Home.vue
@@ -74,7 +74,7 @@
 			return {
 				activeMenuId: '',
 				sysName:'ams 系统',
-				sysVersion: '1.0.5',
+				sysVersion: '1.0.6',
 				collapsed:false,
 				sysUserName: '',
 				sysUserAvatar: '../../static/user.png',
