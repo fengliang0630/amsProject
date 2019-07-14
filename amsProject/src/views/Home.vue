8775efd9fcd7213448f67980a6f9762fabe69b02
冯亮 <135026209@qq.com>
2019-07-14 10:07:28 +0800

    bug处理
    bug处理

--- amsProject/src/views/Home.vue
+++ amsProject/src/views/Home.vue
@@ -75,7 +75,7 @@
 				isRouter: true,
 				activeMenuId: '',
 				sysName:'建设工程规划许可证管理系统',
-				sysVersion: '1.1.1',
+				sysVersion: '1.1.2',
 				collapsed:false,
 				sysUserName: '',
 				sysUserAvatar: '../../static/user.png',
